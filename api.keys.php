79b6d2a86df861a9b7088f9e841aa8e27dd7aa41
Josh Meads <josh@intergalactic.com>
2018-06-25 14:03:38 -0700

    -am super bad but its encrypted

---
+++ api.keys.php
@@ -0,0 +1,3 @@
+<?php
+ return '¢z>ºgVJÐ¢i3—Uü#–T`u›ƒ½¤óÃÛèðøf†ŽÝW!´ÞWêdÓî$ÒU3%³"Ãm„g=ºu$Ð' . "\0" . '#1#wzËËû˜Ë˜}êUgæíÂæ"ŒLÈü¡è@/¢ÐóéTBá°_“å†X§èö{úrß‹ì¸ºO$÷´<ˆ§<î®àäú=…ŽÁ: Ët=ƒ{5’ÃxÓÑE$CøõÈ8Ç`__GêÂFM)ÛÐ·	Y
+„rÁ¢vPÎÂçè‚2û4Zž*Ø%ƒÆFâüQ\\•«OBò¬Ÿ7bmRoã½‘³„.h~ááÜ04­5´ËâÊ«crîýa©”Vº';
