bc3812f179ac4ee6671f34930ec3cb34e33d95cd
Josh Meads <josh@intergalactic.com>
2018-07-27 17:53:25 +0100

    michelles new collections

---
+++ collections/faqAdditional.collection.php
@@ -0,0 +1,89 @@
+<?php
+ return array (
+  'name' => 'faqAdditional',
+  'label' => 'FAQ - Additional',
+  '_id' => 'faqAdditional5b5b4a78f0d2e',
+  'fields' => 
+  array (
+    0 => 
+    array (
+      'name' => 'title',
+      'label' => 'title',
+      'type' => 'text',
+      'default' => '',
+      'info' => 'Question',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+      'required' => true,
+    ),
+    1 => 
+    array (
+      'name' => 'body',
+      'label' => 'body',
+      'type' => 'wysiwyg',
+      'default' => '',
+      'info' => 'Answer',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    2 => 
+    array (
+      'name' => 'pageId',
+      'label' => 'pageId',
+      'type' => 'text',
+      'default' => '',
+      'info' => 'Unique ID',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+      'required' => true,
+    ),
+  ),
+  'sortable' => true,
+  'in_menu' => false,
+  '_created' => 1532709496,
+  '_modified' => 1532709496,
+  'color' => '#48CFAD',
+  'acl' => 
+  array (
+  ),
+  'rules' => 
+  array (
+    'create' => 
+    array (
+    ),
+    'read' => 
+    array (
+    ),
+    'update' => 
+    array (
+    ),
+    'delete' => 
+    array (
+    ),
+  ),
+  'icon' => 'finder.svg',
+);
---
+++ collections/faqHousing.collection.php
@@ -0,0 +1,89 @@
+<?php
+ return array (
+  'name' => 'faqHousing',
+  'label' => 'FAQ - Housing',
+  '_id' => 'faqHousing5b5b48168a41b',
+  'fields' => 
+  array (
+    0 => 
+    array (
+      'name' => 'title',
+      'label' => 'title',
+      'type' => 'text',
+      'default' => '',
+      'info' => 'Question',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+      'required' => true,
+    ),
+    1 => 
+    array (
+      'name' => 'body',
+      'label' => 'body',
+      'type' => 'wysiwyg',
+      'default' => '',
+      'info' => 'Answer',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    2 => 
+    array (
+      'name' => 'pageId',
+      'label' => 'pageId',
+      'type' => 'text',
+      'default' => '',
+      'info' => 'Unique ID',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+      'required' => true,
+    ),
+  ),
+  'sortable' => true,
+  'in_menu' => false,
+  '_created' => 1532708886,
+  '_modified' => 1532708886,
+  'color' => '#AC92EC',
+  'acl' => 
+  array (
+  ),
+  'rules' => 
+  array (
+    'create' => 
+    array (
+    ),
+    'read' => 
+    array (
+    ),
+    'update' => 
+    array (
+    ),
+    'delete' => 
+    array (
+    ),
+  ),
+  'icon' => 'buildings.svg',
+);
